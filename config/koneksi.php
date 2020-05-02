412efba2a68041be46385fa05d73b4a6db16b163
arinifir <55473280+arinifir@users.noreply.github.com>
2020-05-03 02:25:48 +0700

    Initial commit

---
+++ config/koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+
+//koneksi database
+$HostName = "localhost";
+$HostUser = "root";
+$HostPass = "";
+$db = "db_absensi";
+
+?>
---
+++ config/logindos.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+    //memanggil aksi pada koneksi.php
+    include 'koneksi.php';
+    $koneksi = mysqli_connect($HostName,$HostUser,$HostPass,$db);
+ 
+    //deklarasi variabel dengan masing-masing masukkan
+    $nip = $_POST['nip'];
+    $password = $_POST['password'];
+    //cek ke database
+    $cek = "SELECT * from dosen where nip='$nip' and password_dosen='$password'";
+    $prosescek = mysqli_fetch_array(mysqli_query($koneksi,$cek));
+    if(isset($prosescek)){
+        //jika data cocok
+        echo "Data Terkonfirmasi";
+    }else{
+        //jika tidak cocok
+        echo "Email dan Password Salah, Coba Lagi!";
+    }
+
+}else{
+    echo "Cek Lagi";
+}
+?>
